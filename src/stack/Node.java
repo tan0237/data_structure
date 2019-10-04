ee816dd7a3354b9da3665cd8e099889981af38dd
cheng.tan <cheng.tan@yitu-inc.com>
2019-10-04 10:28:17 +0800

    stack

---
+++ src/stack/Node.java
@@ -0,0 +1,8 @@
+package stack;
+
+public interface Node {
+    //获取结点数据域
+    public Object getData();
+    //设置结点数据域
+    public void setData(Object obj);
+}
---
+++ src/stack/StackEmptyException.java
@@ -0,0 +1,7 @@
+package stack;
+
+public class StackEmptyException extends RuntimeException{
+    public StackEmptyException(String err) {
+        super(err);
+    }
+}
